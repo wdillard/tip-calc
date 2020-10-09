030d902f5de89687e29254094dba506e0dbf4753
wdillard <dillardwa@gmail.com>
2020-10-09 14:48:24 -0500

    update

--- tip-calc.js
+++ tip-calc.js
@@ -10,11 +10,19 @@ const totalPartySize = readlineSync.questionInt(
   "How many people are you in your party? "
 );
 
-// Get the % of the tip from the user
-const tipPercentage = readlineSync.questionFloat(
-  "What % would you like to tip? "
-);
+let tipPercentage;
 
+if (totalPartySize >= 6) {
+    // If the party is six or more people then skip
+    // over the percentage and force it to be 18%.
+    tipPercentage = 18;
+  } else {
+    // Get the % of the tip from the user
+    tipPercentage = readlineSync.questionFloat(
+      "What % would you like to tip? "
+    );
+  }
+  
 // Repeat all of those things back to the user.
 console.log(`The total amount of your bill is $${totalAmount.toFixed(2)}`);
 console.log(`The total party size is ${totalPartySize}`);
